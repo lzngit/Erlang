85a173151147ba9045073a03798118984ad33da1
lzn <lznlzn2005@163.com>
2019-02-20 09:19:07 +0800

    Update fibonacci.erl
    fibonacci tail recursive

--- fibonacci.erl
+++ fibonacci.erl
@@ -1,3 +1,10 @@
 -module (fibonacci).
 -export ([number/1]).
 
+number(N) ->
+	number(N, 1, 1).
+
+number(1, _Acc, Bcc) -> Bcc;
+number(2, _Acc, Bcc) -> Bcc;
+number(N, Acc, Bcc) when N > 2 ->
+	number(N-1, Bcc, Acc + Bcc). 
